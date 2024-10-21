dfd990f9422b99fc5c3bdede9336a996f007f9ac
Nabil Putra <nabilputra099123@gmail.com>
2024-10-22 02:52:05 +0700

    menambahkan file Bilangan_Terbesar.py

---
+++ Bilangan_Terbesar.py
@@ -0,0 +1,18 @@
+# Program untuk menentukan bilangan terbesar dari tiga input
+
+# Input tiga bilangan dari pengguna
+a = int(input("Masukkan bilangan pertama: "))
+b = int(input("Masukkan bilangan kedua: "))
+c = int(input("Masukkan bilangan ketiga: "))
+
+# Menentukan bilangan terbesar
+if a > b and a > c:
+    terbesar = a
+elif b > a and b > c:
+    terbesar = b
+else:
+    terbesar = c
+
+# Menampilkan hasil
+print(f"Bilangan terbesar adalah: {terbesar}")
+
